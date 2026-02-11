0252a248b554a0ca79f59c49499174fb11fdf095
112534088-SCSVMV <112534088@kanchiuniv.ac.in>
2026-02-11 02:48:08 -0800

    Add files via upload

---
+++ Program 4 selection sort.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,n,temp,minIndex;
+	
+	printf("enter number of element :");
+	scanf("%d",&n);
+	
+	int arr[n];
+	
+	printf("enter %d elements : \n",n);
+	for(i=0;i<n;i++)
+	   scanf("%d",&arr[i]);
+	
+	for(i=0;i<n-1;i++)
+	{
+		minIndex =1;
+		
+		for(j=i+1;j<n;j++)
+		{
+			if(arr[j]<arr[minIndex])
+			  minIndex =j;
+		}
+		
+		temp = arr[i];
+		arr[i]=arr[minIndex];
+		arr[minIndex] = temp;
+	   }   
+	   
+	   printf("sorted array : \n");
+	   for(i=0;i<n;i++)
+	      printf("%d ",arr[i]);
+	      
+	      return 0;
+}
