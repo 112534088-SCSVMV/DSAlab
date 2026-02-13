d89bf3651043ff4bb50cc56e2911afb21ddb905c
112534088-SCSVMV <112534088@kanchiuniv.ac.in>
2026-02-13 02:12:15 -0800

    Add files via upload

---
+++ Program 5 ( Insertion sort ).c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+	int i,n,key,j;
+	printf("enter number of elements:");
+	scanf("%d",&n);
+	
+	int arr[n];
+	
+	printf("enter %d elements :\n",n);
+	for(i=0;i<n;i++)
+	   scanf("%d",&arr[i]);
+	
+	for(i=1;i<n;i++)
+	{
+		key=arr[i];
+		j=i-1;
+		
+		while(j>=0&& arr [j] >key)
+	  {
+		arr [j+1] =  arr[i];
+		j--;
+	   }
+	  arr [j+1] = key;
+    }
+    printf("sorted array :\n");
+    for(i=0 ; i<n ; i++)
+       printf(" %d ", arr [i]);
+       
+       return 0;
+}
